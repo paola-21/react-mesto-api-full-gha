783f8e3df99162949d982244756ca0da09747223
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2023-07-16 17:03:46 +0300

    fix authorize

--- backend/middlwares/auth.js
+++ backend/middlwares/auth.js
@@ -5,15 +5,15 @@ const auth = (req, res, next) => {
   const { token } = req.cookies;
   let payload;
 
-  // if (!token) {
-  //   next(new TokenError('Необходима авторизация'));
-  // }
+  if (!token) {
+    next(new TokenError('Необходима авторизация'));
+  }
 
   try {
     payload = jwt.verify(token, process.env['JWT_SECRET'], { expiresIn: '7d' });
-  } catch (e) {
-    next(new TokenError('Необходима авторизация'));
-    next(e);
+  } catch (err) {
+    //next(new TokenError('Необходима авторизация'));
+    next(err);
   }
   req.user = payload;
   next();
